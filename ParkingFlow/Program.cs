f0bba3cf4b04af10f21a01c3cb697a6ec2ee9b11
Huba <hieu.dcu@gmail.com>
2025-05-31 13:04:28 +1200

    Update: Time out 30 to 5 minutes

--- ParkingFlow/Program.cs
+++ ParkingFlow/Program.cs
@@ -15,7 +15,7 @@ namespace ParkingFlow
             builder.Services.AddDistributedMemoryCache();
             builder.Services.AddSession(options =>
             {
-                options.IdleTimeout = TimeSpan.FromMinutes(30);
+                options.IdleTimeout = TimeSpan.FromMinutes(5);
                 options.Cookie.HttpOnly = true;
                 options.Cookie.IsEssential = true;
             });
